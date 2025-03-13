be90e6d17d23c9bbb8cc4a5e87d766360a92e708
rameshwer45 <official.hitman4593@gmail.com>
2025-03-13 13:15:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
