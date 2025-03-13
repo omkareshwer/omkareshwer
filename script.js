d618aba43dc21700ed84eebeed3d5c4efce377fe
omkareshwer45 <official.hitman4593@gmail.com>
2025-03-13 21:30:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let images = document.querySelectorAll(".gallery-container img");
     
     images.forEach(img => {
-        img.addEventListener("click", () => {
+        img.addEventListener("click", () => { 
             alert("जय महादेव! यह मंदिर की छवि है।");
         });
     });
