a4d78e4d01aea1d67b7c1980bc681dd383248b2f
omkareshwer45 <official.hitman4593@gmail.com>
2025-03-13 21:43:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+ocument.addEventListener("DOMContentLoaded", function() {
+    let images = document.querySelectorAll(".gallery-container img");
+    
+    images.forEach(img => {
+        img.addEventListener("click", () => { 
+            alert("जय महादेव! यह मंदिर की छवि है।");
+        });
+    });
+});
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
